269d911d660570bad14330564edb3d239b56f7c5
Victor Milovanov <lost@losttech.software>
2017-04-01 21:27:52 -0700

    fixed incorrect layout positioning after monitors going off/on

--- src/Screen/Screen.cs
+++ src/Screen/Screen.cs
@@ -4,48 +4,86 @@
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Interop;
     using LostTech.Stack.Compat;
     using FormsScreen = System.Windows.Forms.Screen;
     using static System.FormattableString;
 
     public sealed class Screen
     {
-        readonly FormsScreen screen;
+        FormsScreen CurrentScreen
+        {
+            get {
+                if (this.dirty) {
+                    var newScreen = FormsScreen.AllScreens.FirstOrDefault(screen => screen.DeviceName == this.device);
+                    if (newScreen != null && newScreen != this.currentScreen) {
+                        this.currentScreen = newScreen;
+                        this.detectorWindow.Left = this.currentScreen.WorkingArea.Left;
+                        this.detectorWindow.Top = this.currentScreen.WorkingArea.Top;
+                        this.detectorWindow.Show();
+                        this.detectorWindow.Hide();
+                        this.dirty = false;
+                    }
+                }
+                return this.currentScreen;
+            }
+        }
+
+        readonly string device;
+        FormsScreen currentScreen;
+        bool dirty = false;
+        readonly Window detectorWindow;
 
         Screen(FormsScreen screen)
         {
-            this.screen = screen ?? throw new ArgumentNullException(nameof(screen));
-            var detectorWindow = new Window {
-                Left = this.screen.WorkingArea.Left,
-                Top = this.screen.WorkingArea.Top,
+            this.currentScreen = screen ?? throw new ArgumentNullException(nameof(screen));
+            this.device = screen.DeviceName;
+            this.detectorWindow = new Window {
+                Left = this.CurrentScreen.WorkingArea.Left,
+                Top = this.CurrentScreen.WorkingArea.Top,
                 ShowInTaskbar = false,
                 Title = screen.DeviceName,
                 WindowStyle = WindowStyle.None,
                 Width = 1,
                 Height = 1,
             };
-            detectorWindow.Show();
+            this.detectorWindow.Show();
             try {
-                this.PresentationSource = PresentationSource.FromVisual(detectorWindow);
+                this.PresentationSource = PresentationSource.FromVisual(this.detectorWindow);
             }
             finally {
-                detectorWindow.Hide();
+                this.detectorWindow.Hide();
             }
+            var window = ((HwndSource) this.PresentationSource);
+            WtsApi.WTSRegisterSessionNotification(window.Handle, NotifySessionFlags.ThisSessionOnly);
+            window.AddHook(OnWindowMessage);
+        }
+
+        IntPtr OnWindowMessage(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_WTSSESSION_CHANGE = 0x02B1;
+            const int WM_DPICHANGED = 0x02E0;
+
+            switch (msg) {
+            case WM_WTSSESSION_CHANGE:
+            case WM_DPICHANGED:
+                this.dirty = true;
+                break;
+            }
+            return IntPtr.Zero;
         }
 
         // TODO: track updates
         public PresentationSource PresentationSource { get; }
         public string ID => Invariant($"{Array.IndexOf(AllScreens.ToArray(), this):D3}");
-        public string Name => Invariant($"{this.ID} ({this.screen.Bounds.Width}x{this.screen.Bounds.Height})");
-        public bool IsPrimary => this.screen.Primary;
+        public string Name => Invariant($"{this.ID} ({this.CurrentScreen.Bounds.Width}x{this.CurrentScreen.Bounds.Height})");
+        public bool IsPrimary => this.CurrentScreen.Primary;
 
-        public static Screen Primary => AllScreens.Single(screen => screen.screen.Primary);
+        public static Screen Primary => AllScreens.Single(screen => screen.CurrentScreen.Primary);
         public static IEnumerable<Screen> AllScreens { get; } =
             new ReadOnlyCollection<Screen>(FormsScreen.AllScreens.Select(formsScreen => new Screen(formsScreen)).ToArray());
 
-        public Rect WorkingArea => this.screen.WorkingArea.ToWPF();
+        public Rect WorkingArea => this.CurrentScreen.WorkingArea.ToWPF();
     }
 }
---
+++ src/Screen/WtsApi.cs
@@ -0,0 +1,18 @@
+﻿namespace LostTech.Windows
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    static class WtsApi
+    {
+        [DllImport("Wtsapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool WTSRegisterSessionNotification(IntPtr hwnd, NotifySessionFlags flags);
+    }
+
+    enum NotifySessionFlags
+    {
+        ThisSessionOnly = 0,
+        AllSessions = 1,
+    }
+}
