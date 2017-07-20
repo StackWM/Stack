0a5fa29d72ca023c9ec254ae0868d25fef74393d
Victor Milovanov <lost@losttech.software>
2017-07-19 22:54:04 -0700

    updated some dependencies + added a dependency on LostTech.Stack.Extensibility 0.1.0

--- src/App/DataBinding/ValueToBoolConverter.cs
+++ src/App/DataBinding/ValueToBoolConverter.cs
@@ -1,32 +1,4 @@
-﻿using System;
-using System.Globalization;
-
-#if XAMARIN
-using Xamarin.Forms;
-using Property = Xamarin.Forms.BindableProperty;
-#else
-using Property = System.Windows.DependencyProperty;
-#endif
-
-#if NETFX || WINDOWS_PHONE
-using System.Windows;
-#elif (NETFX_CORE)
-using Windows.UI.Xaml;
-#endif
-
-
-namespace ValueConverters
+﻿namespace ValueConverters
 {
-    public class ValueToBoolConverter<T> : ValueToBoolConverterBase<T, ValueToBoolConverter<T>>
-    {
-        public override T TrueValue {
-            get => (T)this.GetValue(TrueValueProperty);
-            set => this.SetValue(TrueValueProperty, value);
-        }
-
-        public static readonly Property TrueValueProperty =
-            PropertyHelper.Create<T, ValueToBoolConverter<T>>(nameof(TrueValue));
-    }
-
     public class ValueToBoolConverter: ValueToBoolConverter<object> { }
 }
