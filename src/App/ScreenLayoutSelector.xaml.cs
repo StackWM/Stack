779aee0543a20049857dfe677babfc2841711ccd
Victor Milovanov <lost@losttech.software>
2017-11-01 22:14:25 -0700

    fullscreen layout selector window stub

---
+++ src/App/ScreenLayoutSelector.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LostTech.Stack
+{
+    /// <summary>
+    /// Interaction logic for ScreenLayoutSelector.xaml
+    /// </summary>
+    public partial class ScreenLayoutSelector : Window
+    {
+        public ScreenLayoutSelector()
+        {
+            InitializeComponent();
+        }
+    }
+}
