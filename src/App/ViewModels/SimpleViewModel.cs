86418ba00e44c50315b066d9dd7f6f5464d96698
Victor Milovanov <lost@losttech.software>
2018-03-09 14:46:16 -0800

    + SimpleViewModel.Set

--- src/App/ViewModels/SimpleViewModel.cs
+++ src/App/ViewModels/SimpleViewModel.cs
@@ -12,5 +12,15 @@
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        [NotifyPropertyChangedInvocator]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected void Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null) {
+            if (object.Equals(field, newValue))
+                return;
+
+            field = newValue;
+            this.OnPropertyChanged(propertyName);
+        }
     }
 }
