60ea990c6b56c4e87a3d9e48912822018e9d28d2
Victor Milovanov <lost@losttech.software>
2018-03-11 12:01:42 -0700

    + implemented a trivial window button; task #270

---
+++ src/App/ViewModels/DesignTimeWindow.cs
@@ -0,0 +1,14 @@
+﻿namespace LostTech.Stack.ViewModels
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using LostTech.Stack.Models;
+
+    class DesignTimeWindow : IAppWindow
+    {
+        public string Title { get; } = nameof(DesignTimeWindow);
+        public Task<Exception> Activate() => throw new NotSupportedException();
+        public Task<Exception> Move(Rect targetBounds) => throw new NotSupportedException();
+    }
+}
--- src/App/Zones/WindowButton.xaml.cs
+++ src/App/Zones/WindowButton.xaml.cs
@@ -1,27 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace LostTech.Stack.Zones
+﻿namespace LostTech.Stack.Zones
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using EventHook.Hooks;
+    using LostTech.Stack.Models;
+    using PInvoke;
+
     /// <summary>
     /// Interaction logic for WindowButton.xaml
     /// </summary>
     public partial class WindowButton : UserControl
     {
         public WindowButton() {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            this.Unloaded += this.OnUnloaded;
+            this.Dispatcher.ShutdownStarted += this.DispatcherOnShutdownStarted;
+            this.DataContextChanged += this.OnDataContextChanged;
+            this.windowHook.Activated += this.WindowHookOnActivated;
         }
+
+        void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs) {
+            if (this.Window != null)
+                this.ResetIsForeground(User32.GetForegroundWindow());
+        }
+
+        public IAppWindow Window => this.DataContext as IAppWindow;
+
+        void Window_OnClick(object sender, RoutedEventArgs e) => this.Window?.Activate();
+
+        public bool IsForeground => (bool)this.GetValue(IsForegroundPropertyKey.DependencyProperty);
+        public static readonly DependencyPropertyKey IsForegroundPropertyKey =
+            DependencyProperty.RegisterReadOnly(nameof(IsForeground), typeof(bool), typeof(WindowButton), new PropertyMetadata(false));
+        void WindowHookOnActivated(object sender, WindowEventArgs windowEventArgs) =>
+            this.ResetIsForeground(windowEventArgs.Handle);
+        void ResetIsForeground(IntPtr currentForeground) {
+            this.SetValue(IsForegroundPropertyKey, this.Window.Equals(new Win32Window(currentForeground)));
+        }
+
+        void OnUnloaded(object sender, RoutedEventArgs routedEventArgs) {
+            this.windowHook.Dispose();
+            this.Dispatcher.ShutdownStarted -= this.DispatcherOnShutdownStarted;
+        }
+        void DispatcherOnShutdownStarted(object sender, EventArgs eventArgs) {
+            this.windowHook.Dispose();
+        }
+        readonly WindowHookEx windowHook = new WindowHookEx();
     }
 }
