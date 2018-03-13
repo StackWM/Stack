4a58514451c7aa6024b071abfc8704b7346ffc17
Victor Milovanov <lost@losttech.software>
2018-03-13 13:47:57 -0700

    restore window before activating it, or bringing to the front

--- src/App/Models/Win32Window.cs
+++ src/App/Models/Win32Window.cs
@@ -1,6 +1,7 @@
 ﻿namespace LostTech.Stack.Models
 {
     using System;
+    using System.Runtime.InteropServices;
     using System.Threading.Tasks;
     using System.Windows;
     using PInvoke;
@@ -16,7 +17,9 @@
 
         public async Task<Exception> Move(Rect targetBounds) {
             try {
-                if (GetWindowPlacement(this.Handle).showCmd.HasFlag(WindowShowStyle.SW_MAXIMIZE)) {
+                var windowPlacement = WINDOWPLACEMENT.Create();
+                if (GetWindowPlacement(this.Handle, ref windowPlacement) &&
+                    windowPlacement.showCmd.HasFlag(WindowShowStyle.SW_MAXIMIZE)) {
                     ShowWindow(this.Handle, WindowShowStyle.SW_RESTORE);
                 }
             } catch (Win32Exception) { }
@@ -32,6 +35,31 @@
                     (int)targetBounds.Height, true);
                 return null;
             }
+        }
+
+        public string Title => GetWindowText(this.Handle);
+
+        public Task<Exception> Activate() {
+            this.EnsureNotMinimized();
+            return Task.FromResult(
+                SetForegroundWindow(this.Handle) ? null : (Exception)new Win32Exception());
+        }
+
+        public Task<Exception> BringToFront() {
+            this.EnsureNotMinimized();
+            return Task.FromResult(
+                SetWindowPos(this.Handle, HWND_NOTOPMOST, 0, 0, 0, 0,
+                    SetWindowPosFlags.SWP_NOMOVE | SetWindowPosFlags.SWP_NOACTIVATE |
+                    SetWindowPosFlags.SWP_NOREDRAW | SetWindowPosFlags.SWP_NOSIZE)
+                    ? null
+                    : (Exception)new Win32Exception());
+        }
+
+        Exception EnsureNotMinimized() {
+            if (!IsIconic(this.Handle))
+                return null;
+
+            return ShowWindow(this.Handle, WindowShowStyle.SW_RESTORE) ? null : new Win32Exception();
         }
 
         public bool Equals(Win32Window other) {
@@ -53,5 +81,13 @@
         }
 
         public override int GetHashCode() => this.Handle.GetHashCode();
+
+        [DllImport("User32.dll", SetLastError = true)]
+        static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("User32.dll")]
+        static extern bool IsIconic(IntPtr hwnd);
+
+        static readonly IntPtr HWND_NOTOPMOST = IntPtr.Zero;
     }
 }
