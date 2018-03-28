274b527e6772375faa0214ad3f1e81e434203a4d
Victor Milovanov <lost@losttech.software>
2018-03-28 16:43:59 -0700

    improved window positioning with keyboard; bug #289

---
+++ src/App/Utils/MathUtils.cs
@@ -0,0 +1,11 @@
+﻿namespace LostTech.Stack.Utils
+{
+    using System;
+
+    static class MathUtils
+    {
+        public static bool IsBetween<T>(this T a, T min, T max) where T : IComparable<T> {
+            return a.CompareTo(min) >= 0 && a.CompareTo(max) <= 0;
+        }
+    }
+}
