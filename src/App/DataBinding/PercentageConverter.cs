3de75476cb6af817b1d3d114af8c6546c1c73824
Victor Milovanov <lost@losttech.software>
2018-04-02 10:21:20 -0700

    added PercentageConverter and symmetric Wide layout

---
+++ src/App/DataBinding/PercentageConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace LostTech.Stack.DataBinding
+{
+    using System;
+    using System.Globalization;
+    using ValueConverters;
+
+    public sealed class PercentageConverter : ConverterBase
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (targetType != typeof(double))
+                throw new NotSupportedException();
+
+            double doubleVal = System.Convert.ToDouble(value);
+            double multiplier = System.Convert.ToDouble(parameter);
+            return doubleVal * multiplier / 100.0;
+        }
+    }
+}
