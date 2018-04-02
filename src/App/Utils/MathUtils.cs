0d405018035971b9c52c7e97e7b860169533237c
Victor Milovanov <lost@losttech.software>
2018-04-01 22:46:41 -0700

    fixed keyboard navigation to zero-sized zones; bug #299

--- src/App/Utils/MathUtils.cs
+++ src/App/Utils/MathUtils.cs
@@ -7,5 +7,7 @@
         public static bool IsBetween<T>(this T a, T min, T max) where T : IComparable<T> {
             return a.CompareTo(min) >= 0 && a.CompareTo(max) <= 0;
         }
+
+        public static T AtLeast<T>(this T a, T min) where T : IComparable<T> => a.CompareTo(min) >= 0 ? a : min;
     }
 }
