fcdfe10a8819bc8b6e2130b3282adffa1a60ef44
Victor Milovanov <lost@losttech.software>
2018-04-10 18:00:52 -0700

    added What's New to Help menu

--- src/App/TrayIcon.cs
+++ src/App/TrayIcon.cs
@@ -111,6 +111,7 @@
             var help = new ToolStripMenuItem("Help", image: null) {DisplayStyle = ToolStripItemDisplayStyle.Text};
             help.DropDownItems.Add(Link("Telegram Community","https://t.me/joinchat/HCVquw4yDSmwxky5pxxKZw"));
             help.DropDownItems.Add(Link("Ask a Question", "https://www.allanswered.com/community/s/stack-wm/"));
+            help.DropDownItems.Add(Link("What's New", "https://losttech.software/stack-whatsnew.html"));
             help.DropDownItems.Add("About", image: null, onClick: (_, __) => this.aboutWindow.Show());
             return help;
         }
