ab0f9fe2d38c46810928317b0510ecf7010f3880
Victor Milovanov <lost@losttech.software>
2018-07-19 13:10:58 -0700

    changed UX of WindowGroupListEditor

--- src/App/DataBinding/CopyableObservableCollection.cs
+++ src/App/DataBinding/CopyableObservableCollection.cs
@@ -1,9 +1,12 @@
 ﻿namespace LostTech.Stack.DataBinding
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
+    using JetBrains.Annotations;
     using LostTech.App.DataBinding;
+    using LostTech.Stack.Utils;
 
     public sealed class CopyableObservableCollection<T> : ObservableCollection<T>, ICopyable<CopyableObservableCollection<T>>
         where T:ICopyable<T>
@@ -11,5 +14,9 @@
         public CopyableObservableCollection() { }
         public CopyableObservableCollection(IEnumerable<T> values) : base(values.Select(value => value.Copy())) { }
         public CopyableObservableCollection<T> Copy() => new CopyableObservableCollection<T>(this);
+        public void CopyTo([NotNull] ICollection<T> target) {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            target.AddRange(this.Select(value => value.Copy()));
+        }
     }
 }
--- src/App/Views/WindowGroupListEditor.xaml.cs
+++ src/App/Views/WindowGroupListEditor.xaml.cs
@@ -16,35 +16,33 @@
     {
         public WindowGroupListEditor()
         {
-            this.AddGroupCommand = new DelegateCommand<WindowGroup>(group => this.Groups.Add(group.Name));
+            this.AddGroupCommand = new DelegateCommand<WindowGroup>(group => {
+                    this.Groups.Add(@group.Name);
+                    this.AddGroupCommand.RaiseCanExecuteChanged();
+                },
+                canExecuteMethod: group => group != null && !this.Groups.Contains(group.Name));
 
             this.InitializeComponent();
 
-            NameScope.SetNameScope(this.AvaiableGroupsMenu, NameScope.GetNameScope(this));
+            NameScope.SetNameScope(this.AvaiableGroupsList, NameScope.GetNameScope(this));
         }
 
-        public ICommand AddGroupCommand { get; }
+        public DelegateCommand<WindowGroup> AddGroupCommand { get; }
 
         public IEnumerable<WindowGroup> AvailableGroups {
             get => (IEnumerable<WindowGroup>)this.GetValue(AvailableGroupsProperty);
             set => this.SetValue(AvailableGroupsProperty, value);
         }
 
-
-
         public ICollection<string> Groups {
             get => (ICollection<string>)this.GetValue(GroupsProperty);
             set => this.SetValue(GroupsProperty, value);
         }
 
-        void AddGroupClick(object sender, RoutedEventArgs e)
-        {
-            var addButton = (Button)sender;
-            addButton.ContextMenu.IsOpen = true;
+        void RemoveGroupClick(object sender, RoutedEventArgs e) {
+            this.Groups.Remove((string)this.GroupList.SelectedValue);
+            this.AddGroupCommand.RaiseCanExecuteChanged();
         }
-
-        void RemoveGroupClick(object sender, RoutedEventArgs e)
-            => this.Groups.Remove((string)this.GroupList.SelectedValue);
 
         // Using a DependencyProperty as the backing store for Groups.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty GroupsProperty =
@@ -52,10 +50,13 @@
                 typeof(WindowGroupListEditor), new PropertyMetadata(new ObservableCollection<string>()));
 
 
-
         // Using a DependencyProperty as the backing store for AvailableGroups.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty AvailableGroupsProperty =
             DependencyProperty.Register(nameof(AvailableGroups), typeof(IEnumerable<WindowGroup>),
                 typeof(WindowGroupListEditor), new PropertyMetadata(null));
+
+        void AvaiableGroupsList_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            this.AddGroupCommand.RaiseCanExecuteChanged();
+        }
     }
 }
