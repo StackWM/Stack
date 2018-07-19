700a5fcc558b08c9e153012178415def4ab19a1e
Victor Milovanov <lost@losttech.software>
2018-07-19 13:10:58 -0700

    changed UX of WindowGroupListEditor

--- src/App/DataBinding/CopyableObservableCollection.cs
+++ src/App/DataBinding/CopyableObservableCollection.cs
@@ -1,8 +1,10 @@
 ﻿namespace LostTech.Stack.DataBinding
 {
+    using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
+    using JetBrains.Annotations;
     using LostTech.App.DataBinding;
 
     public sealed class CopyableObservableCollection<T> : ObservableCollection<T>, ICopyable<CopyableObservableCollection<T>>
@@ -11,5 +13,10 @@
         public CopyableObservableCollection() { }
         public CopyableObservableCollection(IEnumerable<T> values) : base(values.Select(value => value.Copy())) { }
         public CopyableObservableCollection<T> Copy() => new CopyableObservableCollection<T>(this);
+        public void CopyTo([NotNull] ICollection<T> target) {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            foreach (var val in this.Select(value => value.Copy()))
+                target.Add(val);
+        }
     }
 }
