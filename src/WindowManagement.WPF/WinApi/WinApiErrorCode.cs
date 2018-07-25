89e38caebc297a0d13e4f702b182c23d428f38b6
Victor Milovanov <lost@losttech.software>
2018-07-25 00:12:16 -0700

    moved core project into submodules

--- src/WindowManagement.WPF/WinApi/WinApiErrorCode.cs
+++
@@ -1,9 +0,0 @@
-﻿// ReSharper disable InconsistentNaming
-namespace LostTech.Stack.WindowManagement.WinApi {
-    public enum WinApiErrorCode
-    {
-        ERROR_ACCESS_DENIED = 5,
-
-        ERROR_INVALID_WINDOW_HANDLE = 1400,
-    }
-}
