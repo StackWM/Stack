89be5314e08def0e2883b47e2aa23daa127ae667
Victor Milovanov <lost@losttech.software>
2018-08-09 18:25:58 -0700

    updated Copyright year

--- src/App/Properties/AssemblyInfo.cs
+++ src/App/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lost Tech LLC")]
 [assembly: AssemblyProduct("Stack")]
-[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2017")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
