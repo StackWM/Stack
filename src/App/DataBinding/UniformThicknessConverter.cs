5ce978952b40933c695961d08db0efec236b65c3
Victor Milovanov <lost@losttech.software>
2019-04-08 13:58:38 -0700

    upgraded ValueConverters package

--- src/App/DataBinding/UniformThicknessConverter.cs
+++ src/App/DataBinding/UniformThicknessConverter.cs
@@ -10,7 +10,7 @@
         protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             if (targetType != typeof(Thickness))
                 throw new NotSupportedException($"Can only convert to {typeof(Thickness).FullName}");
-            
+
             if (value == null)
                 return new Thickness();
 
@@ -21,7 +21,7 @@
                 return new Thickness(f);
             if (value is int i)
                 return new Thickness(i);
-            
+
             throw new NotSupportedException("Value is not of supported type");
         }
     }
