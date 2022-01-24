8ce080a14e503f28b8d55cde8a9d0c38966c9b5a
Victor Nova <lost@losttech.software>
2022-01-23 17:25:28 -0800

    AllReady extension to wait for all layouts to be loaded

---
+++ src/App/Utils/LayoutExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using LostTech.Stack.Zones;
+
+namespace LostTech.Stack.Utils;
+
+static class LayoutExtensions {
+    public static async Task<bool> AllReady(this IEnumerable<ScreenLayout> layouts,
+                                            CancellationToken cancellation = default) {
+        while (!cancellation.IsCancellationRequested) {
+            var layoutElements = layouts.Select(l => l.Layout).ToArray();
+            if (layoutElements.Contains(null)) {
+                await Task.Delay(TimeSpan.FromMilliseconds(250), cancellation);
+            } else {
+                await Task.WhenAll(layoutElements.Select(Layout.GetReady));
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
