0a43fc3b84d5abfe335861318723ae887041ae8b
Victor Nova <lost@losttech.software>
2022-01-23 18:01:41 -0800

    expsed a few schemas (xmlns) for layouts in XAML

---
+++ src/App/Extensibility/Xmlns.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/behavior",
+                           "LostTech.Stack.Behavior")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/extensibility",
+                           "LostTech.Stack.Extensibility")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/extensibility",
+                           "LostTech.Stack.Extensibility",
+                           AssemblyName = "LostTech.Stack.Extensibility")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/extensibility/filters",
+                           "LostTech.Stack.Extensibility.Filters")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/extensibility/filters",
+                           "LostTech.Stack.Extensibility.Filters",
+                           AssemblyName = "LostTech.Stack.Extensibility")]
+
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/all",
+                           "LostTech.Stack.Behavior")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/all",
+                           "LostTech.Stack.Extensibility")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/all",
+                           "LostTech.Stack.Extensibility",
+                           AssemblyName = "LostTech.Stack.Extensibility")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/all",
+                           "LostTech.Stack.Extensibility.Filters")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/all",
+                           "LostTech.Stack.Extensibility.Filters",
+                           AssemblyName = "LostTech.Stack.Extensibility")]
+[assembly: XmlnsDefinition("https://schemas.losttech.software/stack/2022/xaml/all",
+                           "LostTech.Stack.Zones")]
